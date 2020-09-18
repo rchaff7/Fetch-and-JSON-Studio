3ac8540728f7e141becaddb3d093600bef788d0e
Ryan Chaffee <rchaff7@yahoo.com>
2020-09-18 06:05:24 -0500

    completed

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            const container = document.getElementById("container");
+            let astronaut = ""
+            let astronauts = ""
+            for(let i=0; i<json.length; i++){
+                astronaut = json[i]
+                astronauts +=
+                    `<div class="astronaut">
+                        <div class="bio">
+                            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                                <li>Active: ${astronaut.active}</li>
+                                <li>Skills: ${astronaut.skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src=${astronaut.picture}>
+                    </div>`;
+                container.innerHTML = astronauts;
+            }
+        })
+    })
+})
